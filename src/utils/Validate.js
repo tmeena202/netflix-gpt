59ee185b993d77e0d9d85e92aade5e4ca2636736
Tushar Meena <89063728+tmeena202@users.noreply.github.com>
2024-07-03 17:47:09 +0530

    Login page bug fixed

--- src/utils/Validate.js
+++ src/utils/Validate.js
@@ -10,8 +10,24 @@ export const checkValidData = (name, email, password) => {
     /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
 
   if (!isEmailValid) return "Email ID is not valid";
-  if (!isPasswordValid) return "Password is not valid";
   if (!isName) return "Name is not valid";
+  if (password.length < 8) {
+    return "Password must be at least 8 characters long";
+  }
+  if (!/[A-Z]/.test(password)) {
+    return "Password must contain at least one uppercase letter";
+  }
+  if (!/[a-z]/.test(password)) {
+    return "Password must contain at least one lowercase letter";
+  }
+  if (!/\d/.test(password)) {
+    return "Password must contain at least one number";
+  }
+  if (!/[a-zA-Z]/.test(password)) {
+    return "Password must contain at least one letter";
+  }
+  if (!isPasswordValid)
+    return "Password is not Valid. Password must contain 8 digit, first letter capital, one symbol.";
 
   return null;
 };
